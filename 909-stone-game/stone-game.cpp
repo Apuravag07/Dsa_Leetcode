3a3f4a47a92349a82e76f476c05596b949cc5204
Apuravag07 <apuravagrawal07@gmail.com>
2026-08-02 13:32:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (68.24%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // if alice picks up then it is 0 , if bob picks up it is 1 
+        // making a 2d array 
+        return true;
+    }
+};
